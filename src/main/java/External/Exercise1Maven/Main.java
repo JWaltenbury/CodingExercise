175688c2d27ce742aac58bd33688d31bf4ebfa06
Justin <j.waltenbury@hotmail.com>
2021-07-20 16:29:27 -0400

    Code review changes
    -Changed runtime tests to be done using a loop
    -Added logging reference from collab. session
    -pom.xml updated with new dependencies

--- src/main/java/External/Exercise1Maven/Main.java
+++ src/main/java/External/Exercise1Maven/Main.java
@@ -1,10 +1,22 @@
 package External.Exercise1Maven;
 
 import java.util.HashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;	
 
 public class Main {
+	
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
 	public static void main(String[] args)
 	{
+		logger.trace("This is a tracing message");
+		logger.info("This is a information message");
+		logger.warn("This is a warning message");
+		logger.error("This is an error message");
+		//logger.fatal("Don't get in the habit of using this level - only use this if absolutely fatal");
+
+		
 		//Import CSV returns
 		//NOTE: Using this object is out of scope for the exercise
 		@SuppressWarnings("unused")
--- src/test/java/External/Exercise1Maven/RuntimeTest.java
+++ src/test/java/External/Exercise1Maven/RuntimeTest.java
@@ -1,72 +1,56 @@
 package External.Exercise1Maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class RuntimeTest {
 	
-	private HashMap<String, T3Return> returnListNormal;
-	private HashMap<String, T3Return> returnListLarge;
-	private HashMap<String, T3Return> returnListHuge;
-	private HashMap<String, T3Return> initializationMap = new HashMap<String, T3Return>();
+	private HashMap<String, T3Return> importedReturnsMap = new HashMap<String, T3Return>();
+	private List<String> listOfCsvUrls = new ArrayList<>(Arrays.asList("src/main/resources/sampledata.csv", "src/main/resources/sampledata_large.csv", "src/main/resources/sampledata_huge.csv"));
 	T3Return testReturn = new T3Return("T25223424", "TESTNOT", "Toronto", "ON", "123456", "123456");
 	
 	/**
-	 * Preloads all of the CSV files into the class member lists.
+	 * Runs fake calls to properly initialize the HashMap library
 	 */
 	@BeforeClass
 	public void initializeRuntimeLists()
 	{
-		returnListNormal = FileOperations.loadT3ReturnsFromCsv("src/main/resources/sampledata.csv", false); 
-		returnListLarge = FileOperations.loadT3ReturnsFromCsv("src/main/resources/sampledata_large.csv", false); 
-		returnListHuge = FileOperations.loadT3ReturnsFromCsv("src/main/resources/sampledata_huge.csv", false); 
-		
-		System.out.println("\n---------- INITIALIZATION RUNTIME ANALYSIS ("+ initializationMap.size() + " records) ----------");
-		putReturnInMap(initializationMap, testReturn);
-		getReturnFromMap(initializationMap, "T25223424");
-		removeReturnFromMap(initializationMap, "T25223424");
+		System.out.println("\n---------- INITIALIZATION RUNTIME ANALYSIS ("+ importedReturnsMap.size() + " records) ----------");
+		putReturnInMap(importedReturnsMap, testReturn);
+		getReturnFromMap(importedReturnsMap, "T25223424");
+		removeReturnFromMap(importedReturnsMap, "T25223424");
 	}
 	
+  /**
+  * Runtime analysis on the list of CSVs
+  */
   @Test(priority=1)
-  public void sampleDataRuntimeTest()
+  public void performRuntimeAnalysis()
   {	  
-	  System.out.println("\n---------- SAMPLE DATA RUNTIME ANALYSIS ("+ returnListNormal.size() + " records) ----------");
-	  putReturnInMap(returnListNormal, testReturn);
-	  getReturnFromMap(returnListNormal, "T94067732");
-	  removeReturnFromMap(returnListNormal, "T92374709");
+		for(String csvUrl : listOfCsvUrls)
+		{
+			importedReturnsMap = FileOperations.loadT3ReturnsFromCsv(csvUrl, false); 
+			System.out.println("\n---------- SAMPLE DATA RUNTIME ANALYSIS ("+ importedReturnsMap.size() + " records) ----------");
+			putReturnInMap(importedReturnsMap, testReturn);
+			getReturnFromMap(importedReturnsMap, "T25223424");
+			removeReturnFromMap(importedReturnsMap, "T25223424");
+			importedReturnsMap.clear(); // need to empty list for next run through
+		}  
   }
+ 
   
-	
-//	  @Test(priority=1) public void sampleDataRuntimeTest2() {
-//		  System.out.println("\n---------- SAMPLE DATA RUNTIME ANALYSIS ("+
-//		  returnListNormal.size() + " records) ----------");
-//		  putReturnInMap(returnListNormal, testReturn);
-//		  getReturnFromMap(returnListNormal, "T94067732");
-//		  removeReturnFromMap(returnListNormal, "T92374709");
-//	  }
-	 
   
-  @Test(priority=2)
-  public void sampleDataLargeRuntimeTest()
-  {	  
-	  System.out.println("\n---------- SAMPLE DATA (LARGE) RUNTIME ANALYSIS ("+ returnListLarge.size() + " records) ----------");
-	  putReturnInMap(returnListLarge, testReturn);
-	  getReturnFromMap(returnListLarge, "T38395578");
-	  removeReturnFromMap(returnListLarge, "T07655165");
-  }
-  
-  @Test(priority=3)
-  public void sampleDataHugeRuntimeTest()
-  {	  
-	  System.out.println("\n---------- SAMPLE DATA (HUGE) RUNTIME ANALYSIS ("+ returnListHuge.size() + " records) ----------");
-	  putReturnInMap(returnListHuge, testReturn);
-	  getReturnFromMap(returnListHuge, "T59450429");
-	  removeReturnFromMap(returnListHuge, "T75916886");
-  }
-  
-  private static void putReturnInMap(HashMap<String, T3Return> mapToUse, T3Return returnToAdd)
+  /**
+   * Outputs the runtime of adding a return to the given list
+ * @param mapToUse - Map to add to
+ * @param returnToAdd - T3Return to add
+ */
+private static void putReturnInMap(HashMap<String, T3Return> mapToUse, T3Return returnToAdd)
   {
 	  long startTime = System.nanoTime();
 	  mapToUse.put(returnToAdd.getTan(), returnToAdd);
@@ -74,7 +58,12 @@ public class RuntimeTest {
 	  System.out.println("PUT - Complete. Runtime: " + (endTime - startTime) + " nanoseconds");
   }
   
-  private static void getReturnFromMap(HashMap<String, T3Return> mapToUse, String keyToFind)
+  /**
+   * Outputs the runtime of getting a return from the given list
+ * @param mapToUse - Map to search
+ * @param keyToFind - TAN key of the T3Return to find
+ */
+private static void getReturnFromMap(HashMap<String, T3Return> mapToUse, String keyToFind)
   {
 	  long startTime = System.nanoTime();
 	  //System.out.println("\n" + mapToUse.get(keyToFind).toString());
@@ -83,7 +72,12 @@ public class RuntimeTest {
 	  System.out.println("GET - Complete. Runtime: " + (endTime - startTime) + " nanoseconds");
   }
   
-  private static void removeReturnFromMap(HashMap<String, T3Return> mapToUse, String keyToRemove)
+  /**
+   * Outputs the runtime of removing a return from the given list
+ * @param mapToUse - Map to remove from
+ * @param keyToRemove - TAN key of the T3Return to remove
+ */
+private static void removeReturnFromMap(HashMap<String, T3Return> mapToUse, String keyToRemove)
   {
 	  long startTime = System.nanoTime();
 	  mapToUse.remove(keyToRemove);
